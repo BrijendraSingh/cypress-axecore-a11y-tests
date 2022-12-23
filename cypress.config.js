6cb568f2f87bb71d145dc8fb9c26efb78246361c
id_rsa_brij_personal <bps.brijendra@gmail.com>
2022-12-23 13:55:59 +0530

    [Brijendra Singh] added cypress a11y initial commit

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('task', {
+        log(message) {
+          console.log(message)
+          return null
+        },
+    
+        table(message) {
+          console.table(message)
+          return null
+        }
+      })
+    }
+  }
+})
