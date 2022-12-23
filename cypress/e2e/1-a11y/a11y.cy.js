dad687c7a496433f161a17a0c228cfca83f4b71a
id_rsa_brij_personal <bps.brijendra@gmail.com>
2022-12-23 14:11:00 +0530

    [Brijendra Singh] removed commented code

--- cypress/e2e/1-a11y/a11y.cy.js
+++ cypress/e2e/1-a11y/a11y.cy.js
@@ -1,10 +1,6 @@
 /// <reference types="cypress" />
 
 describe('a11y test', () => {
-    beforeEach(() => {
-    //   cy.visit('https://dequeuniversity.com/demo/mars/')
-    })
-  
     it('list down all the a11y feedbacks', () => {
         cy.visit('https://example.cypress.io/todo');
         cy.injectAxe();
